549da5ecefabf72da53f4dfb5f6b61e968f2ccef
StephanieKroese <stephaniekroese@gmail.com>
2013-09-17 11:15:30 -0500

    Homework 1 commit 2
    Fixed several problems after class on 9/12.

--- Hw1Class.py
+++ Hw1Class.py
@@ -1,73 +1,58 @@
 # Homework assignment 1, part 3: Class
 # Stephanie Kroese
-# Due 9/12/2013
-# This contains a class that will return an instance of the burl1_1990 data.
-# It will print the first five values of the SLP and wind components, as well 
-#     as the date and time of the beginning and end of the data
+# Due 9/17/2013
+# This script will create a class for the burl1 data. It will store the pressure,
+#     dates and times, and wind components of the data. It will then display this
+#     information
+
 
 import numpy as np
 from datetime import datetime
-from math import cos
-from math import sin
 
 class burlData():
+    ''' This contains a class that will return an instance of burl1 data.
+    It will print the first five values of the SLP and wind components, as well
+    as the date and time of the beginning and end of the data. Note that not 
+    all years of burl data will have the same columns in the data files, so using 
+    this class for older burl data sets may result in an error.
+    '''
     
     def __init__(self, fileName1):
         self.fileName1 = fileName1
+        f=open(self.fileName1)
         
-    def windCompNorth(self, number):
-        data1=open('burl1_1990.dat')
-        windCompNorth=[]
-        for line in data1.readlines()[1:]:
-            data=line.split()
-            windspeed = float(data[5])
-            windDir = (((float(data[4]))+180)*(3.14/180))
-            windCompNorth.append(windspeed*(cos(windDir)))
-        windCompNorth=np.array(windCompNorth)
-        print windCompNorth[0:number]
-        
-    def windCompEast(self, number):
-        data1=open('burl1_1990.dat')
-        windCompEast=[]
-        for line in data1.readlines()[1:]:
-            data=line.split()
-            windspeed = float(data[5])
-            windDir = (((float(data[4]))+180)*(3.14/180))
-            windCompEast.append(windspeed*(sin(windDir)))
-        windCompEast=np.array(windCompEast)
-        print windCompEast[0:number]
-        
-    def pressure(self, number):
-        data1=open('burl1_1990.dat')
-        pressure=[]
-        for line in data1.readlines()[1:]:
-            data=line.split()
-            pressure.append(float(data[11]))
-        pressure=np.array(pressure)
-        print pressure[0:number]
-        
-    def dateAndTime(self, number):
-        data1=open('burl1_1990.dat')
         dateAndTime=[]
-        for line in data1.readlines()[1:]:
+        pressure = []
+        NorthWindComp = []
+        EastWindComp = []
+        
+        for line in f.readlines()[2:]:
             data=line.split()
-            year = int(data [0])+1900
+            year = int(data [0])
             month = int(data [1])
             day = int(data [2])
             hour = int(data [3])
-            dateAndTime.append(datetime(year, month, day, hour))
-        print dateAndTime[number]
+            minute = int(data[4])
+            
+            dateAndTime.append(datetime(year, month, day, hour, minute))
+            pressure.append(float(data[12]))
+            NorthWindComp.append((float(data[6]))*np.cos((float(data[5]))*3.14/180))
+            EastWindComp.append(-(float(data[6]))*np.sin((float(data[5]))*3.14/180))
+            
+        dateAndTime=np.array(dateAndTime)
+        pressure=np.array(pressure)
+        NorthWindComp=np.array(NorthWindComp)
+        EastWindComp=np.array(EastWindComp)
         
+        self.dateAndTime=dateAndTime
+        self.pressure=pressure
+        self.NorthWindComp=NorthWindComp
+        self.EastWindComp=EastWindComp
+        
+        
+burl1_2011_info=burlData('burl1_2011.dat')
 
-attempt1=burlData('burl1_1990.dat')
-
-print 'Northerly wind components (first five in array):'
-attempt1.windCompNorth(5)
-print '\nEasterly wind components (first five in array):'
-attempt1.windCompEast(5)
-print '\nSea Level Pressure (first five in array): '
-attempt1.pressure(5)
-print '\nBeginning date and time:'
-attempt1.dateAndTime(0)
-print '\nEnding date and time:'
-attempt1.dateAndTime(8718)
+print 'Northerly wind components are: ', burl1_2011_info.NorthWindComp, ' meters per second'
+print 'Easterly wind components are: ', burl1_2011_info.EastWindComp, ' meters per second'
+print 'Pressures are: ', burl1_2011_info.pressure, ' millibars '
+print 'Dates and times are: ', burl1_2011_info.dateAndTime[1], ' through ', burl1_2011_info.dateAndTime[8759]
--- Hw1Function.py
+++ Hw1Function.py
@@ -1,32 +1,36 @@
 # Homework assignment 1, part 2: Function
 # Stephanie Kroese
-# Due 9/12/2013
-# This function will read burl1 data from the file 'burl1_1990.dat'
-#   and put the SLP, wind components, and date/time into arrays. 
-#   It will display (part of) these arrays
+# Due 9/17/2013
+# This function will read burl1 data from a burl1 file and put the pressure, 
+#     wind components, and date/time into arrays. It will then display these 
+#     arrays.
+# Note: I noticed that some of the earlier burl1 files had a slightly different
+#     format for the data. Using this function on an earlier data set may result
+#     in an error (I tried it on the burl1_1990 data and an error occured).
 
 import numpy as np
 from datetime import datetime
 
-def readData1990():
-    f = open('burl1_1990.dat')
+def readData(fileName1):
+    f = open(fileName1)
 
     windCompNorth = []
     windCompEast = []
     pressure = []
     dates = []
     
-    for line in f.readlines()[1:]:
+    for line in f.readlines()[2:]:
         data=line.split()
-        year = (int(data[0])+1900)
+        year = int(data[0])
         month = int(data[1])
         day = int(data[2])
         hour = int(data[3])
+        minutes = int (data[4])
         
-        dates.append(datetime(year, month, day, hour))
-        pressure.append(float(data[11]))
-        windCompNorth.append((float(data[5]))*(np.cos(((float(data[4]))*3.14/180)+3.14)))
-        windCompEast.append((float(data[5]))*(np.sin(((float(data[4]))*3.14/180)+3.14)))
+        dates.append(datetime(year, month, day, hour, minutes))
+        pressure.append(float(data[12]))
+        windCompNorth.append((float(data[6]))*np.cos((float(data[5]))*3.14/180))
+        windCompEast.append(-(float(data[6]))*np.sin((float(data[5]))*3.14/180))
                 
     pressure = np.array(pressure)
     dates = np.array(dates)
@@ -34,10 +38,10 @@ def readData1990():
     windCompEast = np.array(windCompEast)
       
     print 'Pressure (millibars): ', pressure
-    print 'Northward wind components (knots): ', windCompNorth
-    print 'Eastward wind components (knots): ', windCompEast
+    print 'Northward wind components (m/s): ', windCompNorth
+    print 'Eastward wind components (m/s): ', windCompEast
     print 'Date and time start at ', dates[0]
-    print 'Date and time end at', dates[8718]
+    print 'Date and time end at', dates[8759]
 
-readData1990()
+readData('burl1_2011.dat')
 
